f25c533c2eed6910c42be44a5c27da30779490db
topchy <ofirtopchy1@gmail.com>
2023-11-16 15:47:49 +0100

    1

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 ## DIA project
 
-### Part 1
+### Part 1 - Data Acquisition and Preparation
 
-blh blh
+<p> In this part we obtain the research publication datasets. The datasets are in text format.  
+As a prerequisite for <strong>Entity Resolution and model trainin</strong> </p>
+
+We have created a dataset following:
+
+> - paper ID, paper title, author names, publication venue, year of publication
+> 
+> - publications published between 1995 to 2004
+> 
+> - VLDB and SIGMOD venues.
+
+Using Python we achived resullts of thie part are two CSV fills, which would be used as a future datasets. 
+
+<img title="" src="https://media.istockphoto.com/id/97980384/photo/mans-hand-squeezing-half-of-lemon.jpg?s=612x612&w=0&k=20&c=fOwBJdxYux4EpCxA5L3zldTuNcJcdKGQuj9JpQTFM6g=" alt="Mans Hand Squeezing Half Of Lemon Stock Photo - Download Image Now - Lemon  - Fruit, Squeezing, Crushed - iStock" width="136" data-align="right">
--- part1.py
+++ part1.py
@@ -1,15 +1,22 @@
-import numpy as np
-import pandas as pd
 
 databases = []
 
 def prepare(file_name):
+
     # paper ID, paper title, author names, publication venue, and year of publication
     with open(file_name) as f:
+
+        # Split the file into a list of entries
         list_of_files = f.read().split("\n\n")
+
+        # Initialize a dataset with column length as requested schema, row length as number of entries
         databases = [['' for j in range(5)] for i in range(len(list_of_files))]
-        list_of_files = [col.split("\n") for col in list_of_files]
+
+        # Define the order of information in each entry
         identify_string = ["#index", "#*", "#@", "#c", "#t"]
+
+        # Split each entry into lines and extract information based on identify_string
+        list_of_files = [col.split("\n") for col in list_of_files]
         for i in range(len(list_of_files)):
             for col in list_of_files[i]:
                 for j in range(len(identify_string)):
@@ -17,10 +24,12 @@ def prepare(file_name):
                         databases[i][j] = col[len(identify_string[j]):]
                         break
 
+    # Filter the data based on specified criteria
     databases = [data for data in databases if data[-1] != '']
     databases = [data for data in databases if (
         (int(data[-1]) >= 1995) and (int(data[-1]) <= 2004) and (('SIGMOD' in data[-2]) or ('VLDB' in data[-2])))]
 
+    # Write the filtered data to a CSV file
     with open(file_name[:-4]+"_1995_2004.csv", 'w') as f:
         f.write(
             "paper ID; paper title; author names; publication venue; year of publication;\n")
@@ -30,5 +39,6 @@ def prepare(file_name):
                 line = line+col+'; '
             f.write(line+'\n')
 
+# Process the function on our two data files
 for data in ["data/citation-acm-v8.txt", "data/dblp.txt"]:
     prepare("data/dblp.txt")
